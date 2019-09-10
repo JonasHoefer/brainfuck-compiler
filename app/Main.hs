294845c8dd4a5e43e308ace5ee2f6753ce320038
Jonas Hoefer <jonas.hoefer@stud.htwk-leipzig.de>
2019-09-10 16:06:32 +0200

    reformatted code

--- app/Main.hs
+++ app/Main.hs
@@ -3,21 +3,24 @@ module Main where
 import           Parser
 import           Interpreter
 
-import Control.Monad.Trans
-import System.Console.Haskeline
+import           Control.Monad.Trans
+import           System.Console.Haskeline
 
 
 process :: String -> IO ()
 process prog =
-    let Just (ast, _) = runParser program prog
-    in  print (show ast) >> runIntpr runProgram (IntprState (singletonTape 0) ast) >>= putStrLn . fst
+  let Just (ast, _) = runParser program prog
+  in  print (show ast)
+        >>  runIntpr runProgram (IntprState (singletonTape 0) ast)
+        >>= putStrLn
+        .   fst
 
 main :: IO ()
 main = runInputT defaultSettings loop
-  where
+ where
   loop = do
     input <- getInputLine "ready> "
     case input of
-      Nothing -> outputStrLn "Goodbye."
+      Nothing    -> outputStrLn "Goodbye."
       Just input -> liftIO (process input) >> loop
 
