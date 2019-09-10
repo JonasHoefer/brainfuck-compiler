23b67cddb98810769b41932f8190fe1a68beb3f8
Jonas Hoefer <jonas.hoefer@stud.htwk-leipzig.de>
2019-09-10 11:28:31 +0200

    added input loop

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,22 @@ module Main where
 import           Parser
 import           Interpreter
 
+import Control.Monad.Trans
+import System.Console.Haskeline
 
-prog =
-    "++++[>++++<-]>[>+>++>[+++++++>]+++[<]>-]>>>>>>>>-.<<<<.<..+++.<.>>>>.<<<.+++.------.>-.<<+.<------."
 
-
-main :: IO ()
-main =
+process :: String -> IO ()
+process prog =
     let Just (ast, _) = runParser program prog
         (output, _)   = runIntpr runProgram (IntprState (singletonTape 0) ast)
     in  print output
 
+main :: IO ()
+main = runInputT defaultSettings loop
+  where
+  loop = do
+    input <- getInputLine "ready> "
+    case input of
+      Nothing -> outputStrLn "Goodbye."
+      Just input -> (liftIO $ process input) >> loop
 
